d8d9c095622dfd8bbc75ce120af962f66907fda2
abhivemp <abhijitvempati@gmail.com>
2020-02-15 01:47:08 -0500

    Successfully created add() for data structure

--- Data Structure/Main.java
+++ Data Structure/Main.java
@@ -1,12 +1,34 @@
 public class Main {
-    
-    List test = new List();
 
-    // test.add("Abhijit");
-    // test.add("Chetna");
-    // test.add("Lakshmi");
-    // test.add("Surya");
 
-    test.stuff("Abhijit");
+    public static void main(String[] args) {
+        List test = new List();
 
+//     test.add("Abhijit");
+//     test.add("Chetna");
+//     test.add("Lakshmi");
+//     test.add("Surya");
+
+        // test.singlyAdd("Abhijit");
+        // test.singlyAdd("Chetna");
+        // test.singlyAdd("Nanamma");
+        // test.singlyAdd("Thatha");
+        // test.singlyAdd("Lakshmi");
+        // test.singlyAdd("Surya");
+
+
+        // test.print();
+
+        test.add("Thatha");
+        test.add("Surya");
+        test.add("Abhi");
+        
+        test.printcircly();
+        System.out.println("---------");
+        test.remove("Abhi");
+        
+        test.printcircly();
+
+
+    }
 }
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # horse-basketball
 A common street basketball game is HORSE, where a set of players compete to make particular shots. This is a simple tracking application
+
+## Implementation
+
+To make my code implementation easier, I have designed a circular singly linked list. Each node would hold the player name, followed by their score. For this, I have built the custom data structure to suit the requirements for this project. 
+
+
+## UPDATES 
+
+This section will be updated as the project progress.
+
+Currently designing the data structure. Built the add() method. Currently debugging print and remove.
